f8f7cecb85a4ca0dfd93f2914f0e245f95dbda56
Anita Terziska <63020646+Nit4e@users.noreply.github.com>
2023-04-29 17:33:47 +0200

    Project setup

---
+++ src/test/java/mk/ukim/finki/studentproductivityhelperapp/StudentProductivityHelperAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.studentproductivityhelperapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentProductivityHelperAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
