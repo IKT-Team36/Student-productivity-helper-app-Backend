4d4b7acdcf5d69a3e8d1ebc3bf085c3bda7a63f1
Nikola <nikola.sofeski@students.finki.ukim.mk>
2023-05-13 11:23:54 +0200

    constructor of parametar 0 error... fixed

--- src/main/java/mk/ukim/finki/studentproductivityhelperapp/StudentProductivityHelperAppApplication.java
+++ src/main/java/mk/ukim/finki/studentproductivityhelperapp/StudentProductivityHelperAppApplication.java
@@ -1,20 +1,22 @@
 package mk.ukim.finki.studentproductivityhelperapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+
 //@EnableNeo4jRepositories("com.digital.api.repositories")
 //@EnableJpaRepositories(basePackages = "mk.ukim.finki.studentproductivityhelperapp.model")
 @EnableJpaRepositories(basePackages = "mk.ukim.finki.studentproductivityhelperapp.model")
-@EntityScan("mk.ukim.finki.studentproductivityhelperapp.model.*")
-
-
+@EntityScan
+@EnableAutoConfiguration
+@ComponentScan("mk.ukim.finki.studentproductivityhelperapp.repository")
 public class StudentProductivityHelperAppApplication {
 
 	public static void main(String[] args) {
